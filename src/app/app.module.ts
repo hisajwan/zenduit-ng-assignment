a823215f0410c31a33f0b2faf857109994673c06
Himanshu Sajwan <hisajwan@himanshusajwan.local>
2023-11-26 15:24:53 +0530

    ZEN-0 Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleMap, GoogleMapsModule } from '@angular/google-maps';
+import { CardComponent } from './atoms/card/card.component';
+import { DatePickerComponent } from './atoms/date-picker/date-picker.component';
+import { SearchComponent } from './atoms/search/search.component';
+import { SelectComponent } from './atoms/select/select.component';
+import { ToggleComponent } from './atoms/toggle/toggle.component';
+import { MapComponent } from './molecules/map/map.component';
+import { NavigationComponent } from './molecules/navigation/navigation.component';
+import { SubmissionsComponent } from './organisms/submissions/submissions.component';
+import { PageNotFoundComponent } from './organisms/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    DatePickerComponent,
+    SearchComponent,
+    SelectComponent,
+    ToggleComponent,
+    MapComponent,
+    NavigationComponent,
+    SubmissionsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleMapsModule
   ],
-  providers: [],
+  providers: [GoogleMap],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
